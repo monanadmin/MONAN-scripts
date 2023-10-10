93b48b7a432661606ab3b2f4bf50f532788a1901
Denis Eiras <denis.eiras@gmail.com>
2023-10-10 16:30:56 -0300

    MOD7: Showing log folder in script 3

--- egeon/3.run_monan.bash
+++ egeon/3.run_monan.bash
@@ -1,3 +1,9 @@
+#!/bin/bash
+
+# TODO list:
+# - CR: TODO: trazer o script de submissao do modelo para este script
+
+
 export DIRroot=$(pwd)
 export DIRMPAS=${DIRroot}/MPAS
 
@@ -5,8 +11,8 @@ export GREEN='\033[1;32m'  # Green
 export NC='\033[0m'        # No Color
 
 echo -e  "${GREEN}==>${NC} Submitting MPAS and waiting for finish before exit ... \n"
+echo -e  "${GREEN}==>${NC} Logs being generated at ${DIRMPAS}/testcase/runs/ERA5/2021010100/logs ... \n"
 echo -e  "sbatch ${DIRMPAS}/testcase/runs/ERA5/2021010100/mpas_exe.sh"
 sbatch --wait ${DIRMPAS}/testcase/runs/ERA5/2021010100/mpas_exe.sh
-# output files are already checked at mpas_exe.sh
+# output files are checked at mpas_exe.sh
 
-#CR: TODO: trazer o script de submissao do modelo para este script

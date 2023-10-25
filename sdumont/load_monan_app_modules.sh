2cde64ac001792af163d061da7df7c5d4bc338f0
carlosrenatosouza2 <carlos.renato.souza@gmail.com>
2023-10-25 10:54:47 -0300

    script load_monan_app_modules.sh modified:
    Sequana environment set up!

--- sdumont/load_monan_app_modules.sh
+++ sdumont/load_monan_app_modules.sh
@@ -1,8 +1,10 @@
 module purge
-module load pnetcdf/1.12_openmpi-4.1.4_gnu
-module load netcdf/4.9_openmpi-4.1.4_gnu
-export NETCDF=/scratch/app/netcdf/4.9_openmpi-4.1.4_gnu
-export PNETCDF=/scratch/app/pnetcdf/1.12_openmpi-4.1.4_gnu
+module load sequana/current
+module load pnetcdf/1.10_openmpi-2.0_gnu_sequana
+module load netcdf/4.6_openmpi-2.0_gnu_sequana
+
+export PNETCDF=/scratch/app/pnetcdf/1.10_openmpi-2.0_gnu
+export NETCDF=/scratch/app/netcdf/4.6_openmpi-2.0_gnu
 
 module list
 

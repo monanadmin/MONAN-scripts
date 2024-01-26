a3773451dbecfad1381abea80c2536db4c596c1f
deniseiras <denis.eiras@gmail.com>
2024-01-26 15:48:16 -0300

    #387 - fix post - fix 1

--- egeon_oper/scripts/ngrid2latlon.sh
+++ egeon_oper/scripts/ngrid2latlon.sh
@@ -15,21 +15,18 @@ RESOLUTION=${1}
 DATA_INIT=${2}
 DATA_END=${3}
 
-#export dataf=`date -d "${datai:0:8} ${hh}:00 ${FCST} hours" +"%Y%m%d%H"`
-#export final_date=${dataf:0:4}-${dataf:4:2}-${dataf:6:2}_${dataf:8:2}.00.00
-#start_date=${LABELI:0:4}-${LABELI:4:2}-${LABELI:6:2}_${LABELI:8:2}:00:00
-START_DATE_YYYYMMDD = "${DATA_INIT:0:4}-${DATA_INIT:4:2}-${DATA_INIT:6:2}"
-START_HH_MM = "${DATA_INIT:8:2}:00:00"
-
-END_DATE_YYYYMMDD = "${DATA_END:0:4}-${DATA_END:4:2}-${DATA_END:6:2}"
-END_HH_MM = "${DATA_END:8:2}:00:00"
+START_DATE_YYYYMMDD="${DATA_INIT:0:4}-${DATA_INIT:4:2}-${DATA_INIT:6:2}"
+START_HH_MM="${DATA_INIT:8:2}:00:00"
+END_DATE_YYYYMMDD="${DATA_END:0:4}-${DATA_END:4:2}-${DATA_END:6:2}"
+END_HH_MM="${DATA_END:8:2}:00:00"
 
 rm -f include_fields
 cp include_fields.diag include_fields
 rm -f latlon.nc surface.nc
 
-./convert_mpas ../monanprd/x1.${RESOLUTION}.init.nc ../monanprd/diag*nc
 
+
+./convert_mpas ../monanprd/x1.${RESOLUTION}.init.nc ../monanprd/diag*nc
 cdo settunits,hours -settaxis,${START_DATE_YYYYMMDD},${START_HH_MM},1hour latlon.nc surface.nc
 
 rm -f latlon.nc

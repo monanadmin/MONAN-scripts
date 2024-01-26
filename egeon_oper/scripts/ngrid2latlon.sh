5cd796ef4c39259229439d87570f1296dfe887fc
deniseiras <denis.eiras@gmail.com>
2024-01-26 16:00:04 -0300

    #387 - fix post - fix 4

--- egeon_oper/scripts/ngrid2latlon.sh
+++ egeon_oper/scripts/ngrid2latlon.sh
@@ -20,40 +20,38 @@ START_HH="${DATA_INIT:8:2}"
 END_DATE_YYYYMMDD="${DATA_END:0:4}-${DATA_END:4:2}-${DATA_END:6:2}"
 END_HH="${DATA_END:8:2}"
 
-# rm -f include_fields
-# cp include_fields.diag include_fields
-# rm -f latlon.nc surface.nc
+rm -f include_fields
+cp include_fields.diag include_fields
+rm -f latlon.nc surface.nc
 
+./convert_mpas ../monanprd/x1.${RESOLUTION}.init.nc ../monanprd/diag*nc
+cdo settunits,hours -settaxis,${START_DATE_YYYYMMDD},${START_HH}:00,1hour latlon.nc surface.nc
 
-
-echo ./convert_mpas ../monanprd/x1.${RESOLUTION}.init.nc ../monanprd/diag*nc
-echo cdo settunits,hours -settaxis,${START_DATE_YYYYMMDD},${START_HH}:00,1hour latlon.nc surface.nc
-
-# rm -f latlon.nc
+rm -f latlon.nc
 
 #
 # 2. Timeseries for History fields
 #
 
-# rm -f include_fields wind+pw_sfc.nc
-# cp include_fields.history include_fields
+rm -f include_fields wind+pw_sfc.nc
+cp include_fields.history include_fields
 
-echo ./convert_mpas ../monanprd/x1.${RESOLUTION}.init.nc ../monanprd/history.*.nc
+./convert_mpas ../monanprd/x1.${RESOLUTION}.init.nc ../monanprd/history.*.nc
 
-echo cdo settunits,hours -settaxis,${START_DATE_YYYYMMDD},${START_HH}:00,3hour latlon.nc wind+pw_sfc.nc
+cdo settunits,hours -settaxis,${START_DATE_YYYYMMDD},${START_HH}:00,3hour latlon.nc wind+pw_sfc.nc
 
 # rm -f latlon.nc 
 
 #
 # 3.  
 #
-echo ./convert_mpas ../monanprd/history.${END_DATE_YYYYMMDD}_${END_HH}.00.00.nc
+./convert_mpas ../monanprd/history.${END_DATE_YYYYMMDD}_${END_HH}.00.00.nc
 
 #cdo -setreftime,1900-01-01,00:00:00,1day -setdate,1900-01-01 -setcalendar,standard latlon.nc history.2021-01-04_00.00.00.nc
 
 # 24h
-echo cdo settunits,hours -settaxis,${END_DATE_YYYYMMDD},${END_HH}:00,3hour latlon.nc history.${END_DATE_YYYYMMDD}_${END_HH}.00.00.nc
+cdo settunits,hours -settaxis,${END_DATE_YYYYMMDD},${END_HH}:00,3hour latlon.nc history.${END_DATE_YYYYMMDD}_${END_HH}.00.00.nc
 
-# rm -f latlon.nc
+rm -f latlon.nc
 
 exit 0

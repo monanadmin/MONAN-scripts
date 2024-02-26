a54fe6f7da1bafbb589fe7ab54f49c44d9ab395d
rpsouto <rpsouto@lncc.br>
2024-02-26 13:13:34 -0300

    `load_convertmpas_modules.sh`, updated: module load netcdf/4.9.2_openmpi-4.1.6_gnu_sequana

--- sdumont/load_convertmpas_modules.sh
+++ sdumont/load_convertmpas_modules.sh
@@ -2,6 +2,6 @@
 module purge
 export MODULEPATH=/usr/share/Modules/modulefiles:/etc/modulefiles:/scratch/app/modulos
 module load sequana/current
-module load netcdf/4.6_openmpi-2.0_gnu_sequana
+module load netcdf/4.9.2_openmpi-4.1.6_gnu_sequana
 module list
 
